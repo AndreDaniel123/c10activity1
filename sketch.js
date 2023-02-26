6159f1673266a06bc5a7cd2945129c1239af30c3
AndreDaniel123 <126188467+AndreDaniel123@users.noreply.github.com>
2023-02-26 13:59:54 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var array=[15,43,90,7,41,4,3,71,90,87,65,11];
+console.log(array);
+console.log(array.length);
+var sum=array[0]+array[1]+array[2]+array[3]+array[4]+array[5]+array[6]+array[7]+array[8]+array[9]+array[10]+array[11];
+console.log(sum)
+var average =sum/array.length;
+console.log(average);
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
